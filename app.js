61cc7d792cada5bcd962e63cfb94f0076502d4e6
Josiah Rooney <josiahrooney@gmail.com>
2016-07-12 17:57:32 -0700

    update

--- app.js
+++ app.js
@@ -27,13 +27,11 @@ var entityMap = {
     "&": "&amp;",
     "<": "&lt;",
     ">": "&gt;",
-    '"': '&quot;',
-    "'": '&#39;',
     "/": '&#x2F;'
 };
 
 function escapeHtml(string) {
-    return String(string).replace(/[&<>"'\/]/g, function (s) {
+    return String(string).replace(/[&<>\/]/g, function (s) {
         return entityMap[s];
     });
 }
